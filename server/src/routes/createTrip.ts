23df9bf6cc472edba6a23dfa465341098246400e
ngustavo80 <ngustavo80@hotmail.com>
2024-07-13 16:16:01 -0300

    participants, confirm route, cors

--- server/src/routes/createTrip.ts
+++ server/src/routes/createTrip.ts
@@ -3,38 +3,64 @@ import { ZodTypeProvider } from "fastify-type-provider-zod"
 import { z } from "zod"
 import { prisma } from "../lib/prisma"
 import dayjs from "dayjs"
+import localizedFormat from "dayjs/plugin/localizedFormat"
+import "dayjs/locale/pt-br"
 import { getMailClient } from "../lib/mail"
 import nodemailer from "nodemailer"
+
+dayjs.locale('pt-br')
+dayjs.extend(localizedFormat)
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post('/trips',{
     schema: {
       body: z.object({
         destination: z.string().min(4),
-        start_at: z.coerce.date(),
+        starts_at: z.coerce.date(),
         ends_at: z.coerce.date(),
         owner_name: z.string(),
-        owner_email: z.string().email()
+        owner_email: z.string().email(),
+        emails_to_invite: z.array(z.string().email())
       })
     }
   }, async (request) => {
-    const { destination, ends_at, start_at, owner_email, owner_name } = request.body
+    const { destination, ends_at, starts_at, owner_email, owner_name, emails_to_invite } = request.body
 
-    if(dayjs(start_at).isBefore(new Date())) {
+    if(dayjs(starts_at).isBefore(new Date())) {
       throw new Error('Invalid start date. Must be after the actual date.')
     }
 
-    if(dayjs(ends_at).isBefore(start_at)) {
+    if(dayjs(ends_at).isBefore(starts_at)) {
       throw new Error('Invalid date. Must be after the start date.')
     }
 
     const trip = await prisma.trip.create({
       data: {
-        start_at,
+        starts_at,
         destination,
         ends_at,
+        participants: {
+          createMany: {
+            data: [
+              {
+                email: owner_email,
+                name: owner_name,
+                is_owner: true,
+                is_confirmed: true
+              },
+              ...emails_to_invite.map(email => {
+                return { email }
+              }) 
+            ]
+          } 
+        }
       }
     })
+
+    const formattedStartDate = dayjs(starts_at).format('LL')
+    const formattedEndDate = dayjs(ends_at).format('LL')
+
+    const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirmation`
 
     const mail = await getMailClient()
 
@@ -47,8 +73,18 @@ export async function createTrip(app: FastifyInstance) {
         name: owner_name,
         address: owner_email,
       },
-      subject: "Testando envio de emails",
-      html: `<h1>Apenas testando envio de um email :)</h1>`
+      subject: `Confirme sua viagem para ${destination} em ${formattedStartDate}`,
+      html: `
+        <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+          <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong></p>
+          <p></p>
+          <p>Para confirmar sua viagem, clique no link abaixo:</p>
+          <p></p>
+          <p><a href=${confirmationLink}>Confirmar viagem</a></p>
+          <p></p>
+          <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+        </div>
+      `.trim()
     })
 
     console.log(nodemailer.getTestMessageUrl(message))
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,20 @@
 import fastify from "fastify"
+import cors from "@fastify/cors"
 import { createTrip } from "./routes/createTrip"
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
 
 const app = fastify()
+
+app.register(cors, {
+  origin: 'http://localhost:3000',
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip)
+app.register(confirmTrip)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running")
